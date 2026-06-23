92eab0b649593c7338a5a85baca47df0180292e4
siakun <siakun.official@gmail.com>
2026-06-23 21:10:03 +0900

    chore: add OriginalSurvivalShooter reference archive

---
+++ SurvivalShooter/Assets/ThirdParty/OriginalSurvivalShooter/Scripts/Player/DynamicPhysicMaterial.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicPhysicMaterial : MonoBehaviour {
+
+    private PhysicsMaterial pMat;
+
+    private void Start()
+    {
+        pMat = new PhysicsMaterial();
+
+        pMat.dynamicFriction = 0.5f;
+        pMat.frictionCombine = PhysicsMaterialCombine.Minimum;
+
+        pMat.bounciness = 0.5f;
+        pMat.bounceCombine = PhysicsMaterialCombine.Maximum;
+    }
+}
